16f2be25a79fb72174da9bd573021358b6040361
Kacper L <kacper123987@gmail.com>
2020-08-23 15:17:56 -0500

    Improved verify messages

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
+from os import path
 from tiles_maker import make_tiles
 from level_maker import save_level
 
 
 def verify():
     if len(sys.argv) < 2:
-        print("Please input an image path.")
+        print("Usage: main.py image [output]")
+        return False
+    if(not path.exists(sys.argv[1])):
+        print("File %s does not exist" % sys.argv[1])
         return False
     return True
 
