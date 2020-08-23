a98350ec14d983ac7885203c518a855cae41aaa3
Kacper L <kacper123987@gmail.com>
2020-08-22 19:53:35 -0500

    Outputs DAT file.

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ def run():
     img = Image.open(sys.argv[1])
     resized = img.resize((32, 32))
     tiles = getTiles(resized)
-    # TODO: convert resized image to level
+    saveLevel(tiles)
 
 
 def getTiles(img):
-    tiles = [[None] * img.height] * img.width
+    tiles = [None] * img.width * img.height
     for x in range(img.width):
         for y in range(img.height):
-            tiles[x][y] = getClosestTile(img.getpixel((x, y)))
+            tiles[y * img.width + x] = getClosestTile(img.getpixel((x, y)))
     return tiles
 
 
@@ -38,6 +38,38 @@ def getClosestTile(imgColor):
             bestDiff = diff
             bestTile = colorsToTile.get(color)
     return bestTile
+
+
+def saveLevel(tiles):
+    file = open("level.dat", "wb")
+    file.write((0x0002AAAC).to_bytes(4, byteorder="little"))  # magic number
+    # number of levels = 1
+    file.write((0x0001).to_bytes(2, byteorder="little"))
+    # size of level TODO: use RLE
+    file.write((0x042F).to_bytes(2, byteorder="little"))
+    file.write((0x0001).to_bytes(2, byteorder="little"))  # level number = 1
+    file.write((0x0000).to_bytes(2, byteorder="little"))  # time = 0
+    file.write((0x0000).to_bytes(2, byteorder="little"))  # chips = 0
+    file.write((0x0001).to_bytes(2, byteorder="little"))  # map detail
+    file.write((0x0400).to_bytes(2, byteorder="little"))  # first layer
+    for tile in tiles:
+        file.write(tile.value.to_bytes(1, byteorder="little"))
+    file.write((0x000F).to_bytes(2, byteorder="little"))  # second layer
+    # empty bg layer
+    file.write((0xFFFF00FFFF00FFFF00FFFF00FF0400).to_bytes(
+        15, byteorder="big"))
+    # bytes to end of level
+    file.write((0x0012).to_bytes(2, byteorder="little"))
+    file.write((0x03).to_bytes(1, byteorder="little"))  # field 3
+    file.write((0x06).to_bytes(1, byteorder="little"))  # bytes in title
+    file.write(b"Image\0")  # level title
+    file.write((0x07).to_bytes(1, byteorder="little"))  # field 7
+    file.write((0x01).to_bytes(1, byteorder="little"))  # hint length
+    file.write(b"\0")  # hint
+    file.write((0x06).to_bytes(1, byteorder="little"))  # field 6
+    file.write((0x05).to_bytes(1, byteorder="little"))  # password length
+    file.write(b"BDHP\0")  # password
+    file.close()
 
 
 if __name__ == '__main__':
