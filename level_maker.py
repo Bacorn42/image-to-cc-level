cfc08d309ca68f50b9407471d9df8602f0db8175
Kacper L <kacper123987@gmail.com>
2020-08-24 00:58:29 -0500

    Implemented RLE for saving levels

--- level_maker.py
+++ level_maker.py
@@ -17,29 +17,46 @@ def write_set(file, tiles):
 def write_set_data(file):
     file.write((0x0002AAAC).to_bytes(4, byteorder="little"))  # magic number
     # number of levels = 1
-    file.write((0x0001).to_bytes(2, byteorder="little"))
+    file.write((1).to_bytes(2, byteorder="little"))
 
 
 def write_level(file, tiles):
-    write_level_info(file)
-    write_layers(file, tiles)
+    layer = get_layer(tiles)
+    write_level_info(file, len(layer))
+    write_layers(file, layer)
     write_level_data(file)
 
 
-def write_level_info(file):
-    # size of level TODO: use RLE
-    file.write((0x042F).to_bytes(2, byteorder="little"))
-    file.write((0x0001).to_bytes(2, byteorder="little"))  # level number = 1
-    file.write((0x0000).to_bytes(2, byteorder="little"))  # time = 0
-    file.write((0x0000).to_bytes(2, byteorder="little"))  # chips = 0
+def get_layer(tiles):
+    layer = []
+    i = 0
+    while i < len(tiles):
+        count = 1
+        tile = tiles[i]
+        i += 1
+        while i < len(tiles) and tile == tiles[i] and count < 255:
+            i += 1
+            count += 1
+        if count > 3:
+            layer.extend([0xFF, count, tile.value])
+        else:
+            layer.extend([tile.value] * count)
+    return layer
 
 
-def write_layers(file, tiles):
-    file.write((0x0001).to_bytes(2, byteorder="little"))  # map detail
-    file.write((0x0400).to_bytes(2, byteorder="little"))  # first layer
-    for tile in tiles:
-        file.write(tile.value.to_bytes(1, byteorder="little"))
-    file.write((0x000F).to_bytes(2, byteorder="little"))  # second layer
+def write_level_info(file, level_size):
+    file.write((47 + level_size).to_bytes(2, byteorder="little"))
+    file.write((1).to_bytes(2, byteorder="little"))  # level number = 1
+    file.write((0).to_bytes(2, byteorder="little"))  # time = 0
+    file.write((0).to_bytes(2, byteorder="little"))  # chips = 0
+
+
+def write_layers(file, layer):
+    file.write((1).to_bytes(2, byteorder="little"))  # map detail
+    file.write((len(layer)).to_bytes(2, byteorder="little"))  # first layer
+    for byte in layer:
+        file.write(byte.to_bytes(1, byteorder="little"))
+    file.write((15).to_bytes(2, byteorder="little"))  # second layer
     # empty bg layer
     file.write((0xFFFF00FFFF00FFFF00FFFF00FF0400).to_bytes(
         15, byteorder="big"))
@@ -47,13 +64,13 @@ def write_layers(file, tiles):
 
 def write_level_data(file):
     # bytes to end of level
-    file.write((0x0012).to_bytes(2, byteorder="little"))
-    file.write((0x03).to_bytes(1, byteorder="little"))  # field 3
-    file.write((0x06).to_bytes(1, byteorder="little"))  # bytes in title
+    file.write((18).to_bytes(2, byteorder="little"))
+    file.write((3).to_bytes(1, byteorder="little"))  # field 3
+    file.write((6).to_bytes(1, byteorder="little"))  # bytes in title
     file.write(b"Image\0")  # level title
-    file.write((0x07).to_bytes(1, byteorder="little"))  # field 7
-    file.write((0x01).to_bytes(1, byteorder="little"))  # hint length
+    file.write((7).to_bytes(1, byteorder="little"))  # field 7
+    file.write((1).to_bytes(1, byteorder="little"))  # hint length
     file.write(b"\0")  # hint
-    file.write((0x06).to_bytes(1, byteorder="little"))  # field 6
-    file.write((0x05).to_bytes(1, byteorder="little"))  # password length
+    file.write((6).to_bytes(1, byteorder="little"))  # field 6
+    file.write((5).to_bytes(1, byteorder="little"))  # password length
     file.write((0xDBDDD1C900).to_bytes(5, byteorder="big"))  # password BDHP
